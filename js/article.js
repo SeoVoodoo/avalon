8d70e6a120e4f466c3a07e7d3eed267d4414fa07
Kron1410 <anmaslen@gmail.com>
2026-07-02 14:23:37 +0400

    фиксит высоту отступа в article.js

--- js/article.js
+++ js/article.js
@@ -1,18 +1,14 @@
 
 const floatingBlock = document.querySelector('.sidebar-wrap');
 const targetElement = document.querySelector('.article-footer');
-const marginOffset = 680;
-
+const heightElement = floatingBlock.offsetHeight;
+const marginOffset = heightElement + 240;
 
 window.addEventListener('scroll', function() {
 
-    const scrollTop = window.scrollY || document.documentElement.scrollTop;
-    //console.log('scrollTop', scrollTop);
-
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;    
     const upperBound = targetElement.offsetTop - marginOffset;
-    //console.log('upperBound', upperBound);
-
-    
+        
     if (scrollTop > upperBound) {        
         floatingBlock.style.top = `${upperBound}px`;
     } else {
